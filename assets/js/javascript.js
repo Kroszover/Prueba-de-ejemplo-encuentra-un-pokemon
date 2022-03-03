4c263d8935e870eb6bcd02a72995b8e753132085
CamiloLavado <camilolavado@petalmail.com>
2022-03-03 12:49:12 -0300

    Creacion del metodo ajax para consumir la api

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,30 @@
+//Captura del Click
+$(document).ready(function(){
+    $("form").submit(function(event){
+        event.preventDefault()
+
+        let valueInput = $("#pokemonInput").val();
+       
+        //Metodo AJAX jquery
+        $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + valueInput,
+            success: function(data){
+                let nombre = data.name;
+                let image = data.sprites.front_default;
+                let peso = data.weight;
+
+                $("#pokeInfo").html(`  
+                <div class="text-center">
+                <h3>${nombre}</h3>
+                <img src="${image}" alt="">
+                <h6>Peso:${peso}</h6>
+              </div>`)
+             
+            }
+        })
+    })
+})
+
 //Propio arreglo de objetos para calzar datos en canvas.
 let estadisticas = []
 
